596d9999650571a82ae3287706322ab14661cf13
Seongju-Lee <67941526+Seongju-Lee@users.noreply.github.com>
2021-05-26 15:50:38 +0900

    합vs카운트 추가, 함수 유무 추가

--- code_analysis.py
+++ code_analysis.py
@@ -1,6 +1,28 @@
 from pedal import *
 
 
+########################
+# 함수 매칭 유무 확인
+########################
+match = find_match("""
+def summate():
+    __expr__
+""")
 
-print(123)
+if not match:
+    explain("함수가 정의되지 않았습니다.", label="function_missing")
+
+
+########################
+# sum VS count
+########################
+
+assert_equal(call('summate', [1,3,5]), 9) # 임의로 backend에서 인자를 넣어서 맞으면 True, 틀리면 False
+
+
+
+
+########################
+# 다음으로 할 내용은...?
+########################
 
